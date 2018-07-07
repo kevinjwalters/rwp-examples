ba9ed98f2e774456e5851c223c51d4a1509b5783
Kevin J Walters <kjw.github@gmail.com>
2018-07-07 20:40:06 +0100

    More examples of 32bit vs 64bit integer maths plus ending the program with a magic division in silent-32bit-overflow.c

--- examples/silent-32bit-overflow/silent-32bit-overflow.c
+++ examples/silent-32bit-overflow/silent-32bit-overflow.c
@@ -1,4 +1,6 @@
 /*
+ * silent-32bit-overflow.c v1.0
+ *
  * Demonstration of simple C code where a variable holds a duration value
  * which is then increased in resolution but without due consideration
  * for likely maximum values
@@ -48,27 +50,49 @@ void demo3() {
     int i = 0;
     Duration timer_ns = 0;
     int six_us = 6e6;
-    int six_ns = six_us * 1000;
+    int six_ns = six_us * 1000; /* no compiler warning - static tools may detect this */
     /* gcc 4.8.5 warning: overflow in implicit constant conversion [-Woverflow] */
     /* int six_ns = 6000000000000; */
 
     printf("%s\n", __func__);
     timer_ns += six_ns;
-    printf("timer_us=%ld\n", timer_ns, timer_ns);
+    printf("timer_ns=%ld\n", timer_ns, timer_ns);
 }
 
 void demo4() {
+    int i = 0;
+    ReplacementDuration timer_ns = 0;
+    int six_us = 6e6;
+    int64_t six_ns = six_us * 1000;  /* looks reasonable ... */
+
+    printf("%s\n", __func__);
+    timer_ns += six_ns;
+    printf("timer_ns=%ld\n", timer_ns, timer_ns);
+}
+
+void demo5() {
+    int i = 0;
+    ReplacementDuration timer_ns = 0;
+    int six_us = 6e6;
+    int64_t six_ns = six_us * 1000L;  /* note L suffix (LLP64??) */
+
+    printf("%s\n", __func__);
+    timer_ns += six_ns;
+    printf("timer_ns=%ld\n", timer_ns, timer_ns);
+}
+
+void demo6() {
     int i = 0;
     ReplacementDuration timer_ns = 0;
 
     printf("%s\n", __func__);
     for (i=0; i < 5; i++) {
         timer_ns += 500000000000L; /* add half a second */
-        printf("timer_us=%ld\n", timer_ns);
+        printf("timer_ns=%ld\n", timer_ns);
     } 
 }
 
-void demo5() {
+void demo7() {
     int i = 0;
     ReplacementDuration timer_ns = 0;
     OtherDuration localtimer = 0;
@@ -81,9 +105,17 @@ void demo5() {
     if (rarecondition) { 
         localtimer += 1*1000*1000*1000; /* just one more second */
     }
-    printf("timer_us=%u\n", localtimer);
+    printf("timer_ns=%u\n", localtimer);
     timer_ns += localtimer;  /* add half a second */
-    printf("timer_us=" ReplacementDurationFormat "\n", timer_ns);
+    printf("timer_ns=" ReplacementDurationFormat "\n", timer_ns);
+}
+
+
+int demo8() {
+  int about_to_terminate = 0100;  /* is this 100 ? */
+  int could_be_any_value = 0;
+
+  return about_to_terminate / could_be_any_value;
 }
 
 
@@ -93,4 +125,7 @@ int main(int argc, char *argv[]) {
     demo3();
     demo4();
     demo5();
+    demo6();
+    demo7();
+    demo8();
 }
